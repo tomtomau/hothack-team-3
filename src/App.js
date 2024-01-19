381387474b425f2a7991789820c3d4f20fbf7dd6
Tom Newby <me@tomnewby.net>
2024-01-19 21:11:47 +1000

    Adding first info to our webpage!!!

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello world from Team 3
+        </p>
+
+        <p>
+          Fun shit coming soon!!!!
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
