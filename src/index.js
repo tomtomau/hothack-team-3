9af7af07fa320f887e65da10eaa5be89193e18d3
mini109 <derdriumcateer@gmail.com>
2024-01-20 12:51:12 +1000

    Created routes for Artist & Fan pages and did file clean up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Artist from './artist_components/Artist_main';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Fans from "./fan_components/fans";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/artist",
+    element: <Artist />,
+  },
+  {
+    path: "/fans",
+    element: <Fans />,
+  },
+]);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
